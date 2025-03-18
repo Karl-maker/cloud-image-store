ee382b3bfdc46d0c6125473a7a7b735cbd649164
Family <family@Familys-MacBook-Air.local>
2025-03-18 12:17:26 -0400

    fix: remove bad import

--- src/application/services/video/i.mp4.to.hls.service.ts
+++ src/application/services/video/i.mp4.to.hls.service.ts
@@ -1,4 +1,4 @@
-import { ObjectDataType } from "../../../domain/types/types/object.data.type";
+import { ObjectDataType } from "../../../domain/types/object.data.type";
 
 export default interface IMp4ToHlsService {
     convert: (key: string, cb: (err: null | Error, data?: ObjectDataType) => Promise<void>) => Promise<void>;
